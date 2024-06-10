46981f1f078b21ed9bf605ff332bca1ad487af8e
Prathima Shenoy <Prathimapai.88@gmail.com>
2024-06-10 09:44:34 +0100

    Issue Fixes

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import UserIcon from './../common/component/UserIcon';
+import UserIcon from '../common/component/UserIcon';
 import TabsComponent from './TabsComponent';
-import Spinner from './../common/component/Spinner';
-import { RESOURCES_URL } from "./../common/constants/apiConstants";
+import Spinner from '../common/component/Spinner';
+import { RESOURCES_URL } from "../common/constants/apiConstants";
 import useAPI from "../common/utils/useAPI";
 
-const UserDetails = () => {
+interface User {
+  name: string;
+}
+
+const UserDetails: React.FC = () => {
   const { id } = useParams();
   const { data: userData, loading, error } = useAPI(`${RESOURCES_URL}/${id}`);
 
-  if (loading) return <Spinner/>;
+  if (loading) return <Spinner />;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <div>
       {userData ? (
         <div>
-           <UserIcon details={userData}/>
-           <TabsComponent details={userData} id={id}/>
-           
+          {userData && <UserIcon details={userData} />}
+          {id && <TabsComponent details={userData} id={id} />}
         </div>
       ) : (
         <p>No user data found.</p>
